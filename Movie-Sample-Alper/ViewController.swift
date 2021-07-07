363446a626d424c4b57bdd2672f22b7216652974
Alper Tufan <extern-alperhayrettin.tufan@allianz.com.tr>
2021-07-07 17:14:38 +0300

    Initial Commit

---
+++ Movie-Sample-Alper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie-Sample-Alper
+//
+//  Created by Alper Tufan on 7.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
