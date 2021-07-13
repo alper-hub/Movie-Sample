dae75f63e6b81989b5ab748ada6ad22503662e18
Alper Tufan <extern-alperhayrettin.tufan@allianz.com.tr>
2021-07-13 11:55:51 +0300

    - Refactoring

--- Movie-Sample-AlperTests/Movie_Sample_AlperTests.swift
+++ Movie-Sample-AlperTests/Movie_Sample_AlperTests.swift
@@ -10,39 +10,16 @@ import XCTest
 
 class Movie_Sample_AlperTests: XCTestCase {
 
-    var sut: URLSession!
 
     override func setUpWithError() throws {
-        try super.setUpWithError()
-          sut = URLSession(configuration: .default)
         
     }
 
     override func tearDownWithError() throws {
-        sut = nil
-         try super.tearDownWithError()
     }
     
     func testValidApiCallGetsHTTPStatusCode200() throws {
-      // given
-      let urlString =
-        "https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=fd2b04342048fa2d5f728561866ad52a&page=1"
-      let url = URL(string: urlString)!
-      let promise = expectation(description: "Status code: 200")
-      let dataTask = sut.dataTask(with: url) { _, response, error in
-        if let error = error {
-          XCTFail("Error: \(error.localizedDescription)")
-          return
-        } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
-          if statusCode == 200 {
-            promise.fulfill()
-          } else {
-            XCTFail("Status code: \(statusCode)")
-          }
-        }
-      }
-      dataTask.resume()
-      wait(for: [promise], timeout: 5)
+ 
     }
 
     func testExample() throws {
