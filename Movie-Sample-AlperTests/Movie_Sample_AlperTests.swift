c415e0db846edfe2dd3e63f36f0e7d03003f602d
Alper Tufan <extern-alperhayrettin.tufan@allianz.com.tr>
2021-07-14 10:41:56 +0300

    - refactoring

--- Movie-Sample-AlperTests/Movie_Sample_AlperTests.swift
+++ Movie-Sample-AlperTests/Movie_Sample_AlperTests.swift
@@ -10,16 +10,12 @@ import XCTest
 
 class Movie_Sample_AlperTests: XCTestCase {
 
-
     override func setUpWithError() throws {
-        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
-    }
-    
-    func testValidApiCallGetsHTTPStatusCode200() throws {
- 
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() throws {
